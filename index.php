b584b035178210c2a81529390d10195f65d9f972
haganicolau <haganicolau@gmail.com>
2017-06-27 10:33:26 -0300

    correções

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use OlxAPI\Olx as Olx;
+require('vendor/autoload.php');
+
+$olx = new olx(array(
+	'response_type'=>'{code}',
+	'client_id'=>'{client_id}',
+    'client_secret'=>'{client_secret}',
+    'redirect_uri' => '{redirect_uri}',
+    'state'=>'{state}',
+    'code'=>'{code}'
+));
+
+$url = $olx->getLoginUrl();
+$token = $olx->getToken();
