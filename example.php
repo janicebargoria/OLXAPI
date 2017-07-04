30c3622533fc704255af7966face3d53c7c702f8
haganicolau <haganicolau@gmail.com>
2017-07-04 15:20:08 -0300

    adicionando exemplo.php

--- example.php
+++ example.php
@@ -3,14 +3,16 @@
 use OlxAPI\Olx as Olx;
 require('vendor/autoload.php');
 
-$olx = new olx(array(
-	'response_type'=>'{code}',
-	'client_id'=>'{client_id}',
-    'client_secret'=>'{client_secret}',
-    'redirect_uri' => '{redirect_uri}',
-    'state'=>'{state}',
-    'code'=>'{code}'
-));
+$olx = new olx(
+	array(
+		'response_type'=>'{code}',
+		'client_id'=>'{client_id}',
+	    'client_secret'=>'{client_secret}',
+	    'redirect_uri' => '{redirect_uri}',
+	    'state'=>'{state}',
+	    'code'=>'{code}'
+	)
+);
 
 $url = $olx->getLoginUrl();
 $token = $olx->getToken();
